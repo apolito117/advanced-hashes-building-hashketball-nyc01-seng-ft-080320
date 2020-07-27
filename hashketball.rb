4a72724f728900c0e03f68a1be91b900108d5755
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-27 05:14:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
   the_hash = {}
 
   the_hash[:home] = {}
-  the_hash[:home][:team_name] = "Brooklan Nets"
+  the_hash[:home][:team_name] = "Brooklyn Nets"
   the_hash[:home][:colors] = ["Black", "White"]
 
   the_hash[:home][:players] = []
